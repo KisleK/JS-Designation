138ff1b1d3da3e8510be70e357637ac67918fc2a
Kyle Kuhn <kkuhn@dealereprocess.local>
2020-01-18 11:46:34 -0600

    added third option and JS file

---
+++ where.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    document.getElementById('myButton').addEventListener('click', respond);
+
+    function respond(e) {
+        alert('Clicked script tag js')
+    }
+};
